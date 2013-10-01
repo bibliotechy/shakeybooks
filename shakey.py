8c83dc3204917e70f4e0be1f80bc49c4257a52ec
Chad Nelson <chadbnelson@gmail.com>
2013-10-01 17:06:53 -0400

    trying to make this work with redis. Grrrr.

--- shakey.py
+++ shakey.py
@@ -1,4 +1,5 @@
-
+import os
+import redis
 from random import choice, randint
 
 class MarkyMarkov():
@@ -7,7 +8,9 @@ class MarkyMarkov():
         self.nonword = "\n"
         self.stopwords = (".","!","?",)
         self.limit = limit
-        self.table = {}
+        redis_url = "localhost"
+        #redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+        self.redis = redis.Redis(redis_url)
         self.text = text
 
     def setupDB(self):
@@ -15,19 +18,19 @@ class MarkyMarkov():
         for word in self.text.split():
             if word[-1] in self.stopwords:
                 word = word[0:-1]
-            self.table.setdefault((w1, w2), []).append(word)
+            self.redis.sadd((w1, w2), word)
             w1, w2 = w2, word
-        self.table.setdefault((w1, w2), []).append(word)
+        self.redis.sadd((w1, w2), self.nonword)
 
 
     def build_string(self, seed = None):
         if not seed:
             #get the first three words
-            seed = choice(list(self.table.keys()))
-            w1, w2 = seed
-            sentence = ' '.join([w1,w2])
+            seed = self.redis.randomkey()
+            w1, w2 = seed.translate(None, "()',").split()
+            sentence = ' '.join([w1, w2])
             while len(sentence) < self.limit:
-                seed_word = self.table[(w1,w2)][randint(0, len(self.table[w1,w2])-1)]
+                seed_word = self.redis.srandmember("('%s', '%s')".format(w1, w2))
                 sentence += " " + (seed_word)
                 w1, w2 = w2, seed_word
         return sentence.capitalize()
