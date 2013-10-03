a2c6fe8b0cb9f987cf545343a3a4347bb12bfd79
Chad Nelson <chadbnelson@gmail.com>
2013-10-03 17:05:26 -0400

    Redis working, but too short?

--- shakey.py
+++ shakey.py
@@ -4,7 +4,7 @@ from random import choice, randint
 
 class MarkyMarkov():
 
-    def __init__(self, text, limit=140):
+    def __init__(self, text=None, limit=140):
         self.nonword = "\n"
         self.stopwords = (".","!","?",)
         self.limit = limit
@@ -15,10 +15,8 @@ class MarkyMarkov():
 
     def setupDB(self):
         w1, w2 = self.nonword * 2
-        for word in self.text.split():
-            if word[-1] in self.stopwords:
-                word = word[0:-1]
-            self.redis.sadd((w1, w2), word)
+        for word in self.text.decode().split():
+            self.redis.sadd("{!s}:{!s}".format(w1,w2), word.strip("'\""))
             w1, w2 = w2, word
         self.redis.sadd((w1, w2), self.nonword)
 
@@ -27,11 +25,22 @@ class MarkyMarkov():
         if not seed:
             #get the first three words
             seed = self.redis.randomkey()
-            w1, w2 = seed.translate(None, "()',").split()
-            sentence = ' '.join([w1, w2])
+            w1, w2 = seed.decode().split(":")
+            sentence = ' '.join([w1, w2]).capitalize()
+            next_word = False
             while len(sentence) < self.limit:
-                seed_word = self.redis.srandmember("('%s', '%s')".format(w1, w2))
-                sentence += " " + (seed_word)
+                seed_word = self.redis.srandmember("{}:{}".format(w1, w2))
+                if seed_word:
+                    word = seed_word.decode()
+                    if next_word:
+                        word = word.capitalize()
+                        next_word = False
+                    if word[-1] in self.stopwords:
+                        next_word = True
+                else:
+                    word = ''
+                sentence += " " + word
+
                 w1, w2 = w2, seed_word
-        return sentence.capitalize()
+            return sentence
 
