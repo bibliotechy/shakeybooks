a5d6462b3a9287a2ac3daa5a5a005a4b649f491c
Chad Nelson <chadbnelson@gmail.com>
2013-10-03 22:47:40 -0400

    Fix redis failing issues

--- shakey.py
+++ shakey.py
@@ -4,9 +4,9 @@ from random import choice, randint
 
 class MarkyMarkov():
 
-    def __init__(self, text=None, limit=140):
+    def __init__(self, text=None, limit=None):
         self.nonword = "\n"
-        self.stopwords = (".","!","?",)
+        self.stopwords = (".","!","?",":")
         self.limit = limit
         redis_url = "localhost"
         #redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
@@ -22,25 +22,29 @@ class MarkyMarkov():
 
 
     def build_string(self, seed = None):
+        if not self.limit:
+            limit = randint(90,140)
         if not seed:
             #get the first three words
             seed = self.redis.randomkey()
             w1, w2 = seed.decode().split(":")
-            sentence = ' '.join([w1, w2]).capitalize()
-            next_word = False
-            while len(sentence) < self.limit:
+            sentence = ' '.join([w1.capitalize(), w2])
+            after_stopword = False
+            while len(sentence) < limit:
                 seed_word = self.redis.srandmember("{}:{}".format(w1, w2))
                 if seed_word:
                     word = seed_word.decode()
-                    if next_word:
+                    seed_word = seed_word.decode()
+                    if after_stopword:
                         word = word.capitalize()
-                        next_word = False
                     if word[-1] in self.stopwords:
-                        next_word = True
+                        after_stopword = True
+                    else:
+                        after_stopword = False
+                    sentence += " " + word
+                if seed_word and w2:
+                    w1, w2 = w2, seed_word
                 else:
-                    word = ''
-                sentence += " " + word
-
-                w1, w2 = w2, seed_word
+                    return sentence
             return sentence
 
